a88c5da8ec265ad8f10ecd69f842526522dd5af7
Olga <dergach.13@ya.ru>
2017-03-23 18:47:23 +0300

    Level_1

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Он так её мучит, как будто растит жену.");
+        System.out.println("Он ладит её под себя, под свои пороки,");
+        System.out.println("Привычки, стахи, веснушчатость, рыжину.");
+        System.out.println("Муштрует, мытарит, холит, даёт уроки.");
+    }
+}
